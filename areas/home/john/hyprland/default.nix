fd5608c5bd8361ace84c38860bc15b70bb858ae8
Boris Polyakov <thewaterbox@yandex.ru>
2025-01-13 21:23:57 +0300

    fix noita

--- areas/home/john/hyprland/default.nix
+++ areas/home/john/hyprland/default.nix
@@ -83,7 +83,7 @@ in {
           [
             "XDG_SESSION_TYPE, wayland"
             "GDK_BACKEND, wayland,x11"
-            "SDL_VIDEODRIVER, wayland"
+            # "SDL_VIDEODRIVER, wayland"
             "CLUTTER_BACKEND, wayland"
             "QT_QPA_PLATFORM, wayland;xcb"
             "HYPRCURSOR_THEME, rose-pine-hyprcursor"
