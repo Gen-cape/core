6a114b00c4b7934c8adeddcbe38698daa5ce31c7
Boris Polyakov <thewaterbox@yandex.ru>
2025-03-18 12:50:03 +0300

    Vscode: remove garbage

--- areas/home/john/unordered/vscode.nix
+++ areas/home/john/unordered/vscode.nix
@@ -47,59 +47,6 @@
         ++ [
           pkgs.vscode-extensions."2gua".rainbow-brackets
         ];
-      #++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      #  {
-      #    name = "copilot-nightly";
-      #    publisher = "github";
-      #    version = "1.67.7949";
-      #    sha256 = "sha256-ZtUqQeWjXmTz49DUeYkuqSTdVHRC8OfgWv8fuhlHDVc=";
-      #  }
-      #  {
-      #    name = "vscode-chromium-vector-icons";
-      #    publisher = "adolfdaniel";
-      #    version = "1.0.2";
-      #    sha256 = "sha256-Meo53e/3jUP6YDEXOA/40xghI77jj4iAQus3/S8RPZI=";
-      #  }
-      #];
-      #userSettings = {
-      #  "workbench.iconTheme" = "material-icon-theme";
-      #  "workbench.colorTheme" = "Catppuccin Macchiato";
-      #  "catppuccin.accentColor" = "mauve";
-      #  "editor.fontFamily" = "JetBrainsMono Nerd Font, Material Design Icons, 'monospace', monospace";
-      #  "editor.fontSize" = 16;
-      #  "editor.fontLigatures" = true;
-      #  "workbench.fontAliasing" = "antialiased";
-      #  "files.trimTrailingWhitespace" = true;
-      #  "terminal.integrated.fontFamily" = "JetBrainsMono Nerd Font Mono";
-      #  "window.titleBarStyle" = "custom";
-      #  "terminal.integrated.automationShell.linux" = "nix-shell";
-      #  "terminal.integrated.defaultProfile.linux" = "zsh";
-      #  "terminal.integrated.cursorBlinking" = true;
-      #  "terminal.integrated.enableBell" = false;
-      #  "editor.formatOnPaste" = true;
-      #  "editor.formatOnSave" = true;
-      #  "editor.formatOnType" = false;
-      #  "editor.minimap.enabled" = false;
-      #  "editor.minimap.renderCharacters" = false;
-      #  "editor.overviewRulerBorder" = false;
-      #  "editor.renderLineHighlight" = "all";
-      #  "editor.inlineSuggest.enabled" = true;
-      #  "editor.smoothScrolling" = true;
-      #  "editor.suggestSelection" = "first";
-      #  "editor.guides.indentation" = true;
-      #  "editor.guides.bracketPairs" = true;
-      #  "editor.bracketPairColorization.enabled" = true;
-      #  "window.nativeTabs" = true;
-      #  "window.restoreWindows" = "all";
-      #  "window.menuBarVisibility" = "toggle";
-      #  "workbench.panel.defaultLocation" = "right";
-      #  "workbench.editor.tabCloseButton" = "left";
-      #  "workbench.startupEditor" = "none";
-      #  "workbench.list.smoothScrolling" = true;
-      #  "security.workspace.trust.enabled" = false;
-      #  "explorer.confirmDelete" = false;
-      #  "breadcrumbs.enabled" = true;
-      #};
     };
   };
 }
