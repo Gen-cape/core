60540b62b13b612b9e3bab9ce79dabfd2bb1cb95
Boris Polyakov <thewaterbox@yandex.ru>
2025-04-02 10:59:01 +0300

    jj-fzf: transition to nixpkgs

--- areas/home/john/unordered/riptide.nix
+++ areas/home/john/unordered/riptide.nix
@@ -1,11 +1,11 @@
 {inputs', ...}: {
   home.packages = [
     inputs'.riptide.packages.sttt
-    inputs'.riptide.packages.jujutsu-fzf
     inputs'.riptide.packages.drvinter
     inputs'.riptide.packages.tmsufolders
     inputs'.riptide.packages.tmsufs
     inputs'.riptide.packages.quest
+    inputs'.riptide.packages.jj-fzf
 
     inputs'.riptide.packages.ghostty
     inputs'.riptide.packages.toml-bombadil
