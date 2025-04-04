42b546f6c0d31be2ec9fc5e263e745ee81b1ae21
Boris Polyakov <thewaterbox@yandex.ru>
2025-04-04 20:40:07 +0300

    Cleanup: remove unused kernel code

--- hosts/skald/modules/kernel.nix
+++ hosts/skald/modules/kernel.nix
@@ -1,18 +1,5 @@
-{
-  config,
-  inputs',
-  ...
-}: {
+{inputs', ...}: {
   boot = {
-    # initrd.availableKernelModules = ["nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod"];
-    # initrd.kernelModules = [];
-    # kernelModules = ["v4l2loopback"];
-    # extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
-    # extraModprobeConfig = ''
-    #   options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-    # '';
     kernelPackages = inputs'.chaotic.legacyPackages.linuxPackages_cachyos;
   };
-
-  # services.scx.enable = true;
 }
