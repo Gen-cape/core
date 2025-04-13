47f61f6b4c8cdfd5230d1c5f4523bb81c702b0a9
Boris Polyakov <thewaterbox@yandex.ru>
2025-04-14 01:45:00 +0300

    associations: fix zen mimeapps

--- areas/home/john/unordered/associations.nix
+++ areas/home/john/unordered/associations.nix
@@ -26,15 +26,15 @@
     "x-scheme-handler/https       " = "zen-twilight.desktop";
     "x-scheme-handler/tg          " = "org.telegram.desktop.desktop";
     "x-scheme-handler/tonsite     " = "org.telegram.desktop.desktop";
-    "x-scheme-handler/unknown     " = "zen_twilight.desktop ";
-    "x-scheme-handler/chrome      " = "zen-twilight.desktop ";
-    "text/html                    " = "zen-twilight.desktop ";
-    "application/x-extension-htm  " = "zen-twilight.desktop ";
-    "application/x-extension-html " = "zen-twilight.desktop ";
-    "application/x-extension-shtml" = "zen-twilight.desktop ";
-    "application/xhtml+xml        " = "zen-twilight.desktop ";
-    "application/x-extension-xhtml" = "zen-twilight.desktop ";
-    "application/x-extension-xht  " = "zen-twilight.desktop ";
+    "x-scheme-handler/unknown     " = "zen_twilight.desktop";
+    "x-scheme-handler/chrome      " = "zen-twilight.desktop";
+    "text/html                    " = "zen-twilight.desktop";
+    "application/x-extension-htm  " = "zen-twilight.desktop";
+    "application/x-extension-html " = "zen-twilight.desktop";
+    "application/x-extension-shtml" = "zen-twilight.desktop";
+    "application/xhtml+xml        " = "zen-twilight.desktop";
+    "application/x-extension-xhtml" = "zen-twilight.desktop";
+    "application/x-extension-xht  " = "zen-twilight.desktop";
 
     "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
     "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
