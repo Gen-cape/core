a8c404d58da0d6de0729da676e5fc229aa89beb4
Boris Polyakov <thewaterbox@yandex.ru>
2025-06-12 11:17:40 +0300

    tailscale: optimize routing

--- hosts/kitsune/tailscale.nix
+++ hosts/kitsune/tailscale.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "server";
@@ -6,14 +10,30 @@
     extraUpFlags = [
       "--advertise-exit-node"
     ];
+    interfaceName = "userspace-networking";
   };
 
   environment.systemPackages = [
     config.services.tailscale.package
+
+    pkgs.ethtool
+    pkgs.networkd-dispatcher
   ];
 
   networking.firewall = {
     trustedInterfaces = [config.services.tailscale.interfaceName];
     allowedTCPPorts = [config.services.tailscale.port];
+    allowedUDPPorts = [config.services.tailscale.port]; # Use the dynamic Tailscale port
+  };
+
+  # Configure networkd-dispatcher to set UDP GRO settings on boot
+  services.networkd-dispatcher = {
+    enable = true;
+    rules."50-tailscale" = {
+      onState = ["routable"];
+      script = ''
+        ${pkgs.ethtool}/bin/ethtool -K ens1 rx-udp-gro-forwarding on rx-gro-list off
+      '';
+    };
   };
 }
