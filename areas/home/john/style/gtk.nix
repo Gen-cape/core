983ce83d0ac0970c8c0cda2b2f75df5a8e86f855
Boris Polyakov <thewaterbox@yandex.ru>
2025-10-11 16:13:52 +0300

    theming: remove unnecessary

--- areas/home/john/style/gtk.nix
+++ areas/home/john/style/gtk.nix
@@ -8,19 +8,10 @@
     theme = {
       name = lib.mkForce "Sweet-Dark";
       package = lib.mkForce pkgs.sweet;
-
-      # name = "WhiteSur-Dark-solid-hdpi";
-      # package = pkgs.whitesur-gtk-theme;
-
-      # name = "Orchis-Dark";
-      # package = pkgs.orchis-theme;
     };
     iconTheme = {
       name = "Sweet-Rainbow";
       package = pkgs.sweet-folders;
-
-      # name = "WhiteSur-dark";
-      # package = pkgs.whitesur-icon-theme;
     };
   };
 }
