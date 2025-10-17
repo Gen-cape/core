273a51840f02a07db40539f8e6d8d1d7ad46f5ff
Boris Polyakov <thewaterbox@yandex.ru>
2025-10-17 13:08:11 +0300

    riptide: remove sttt

--- areas/home/john/unordered/riptide.nix
+++ areas/home/john/unordered/riptide.nix
@@ -1,6 +1,6 @@
 {inputs', ...}: {
   home.packages = [
-    inputs'.riptide.packages.sttt
+    # inputs'.riptide.packages.sttt
     inputs'.riptide.packages.drvinter
     inputs'.riptide.packages.tmsufolders
     inputs'.riptide.packages.tmsufs
