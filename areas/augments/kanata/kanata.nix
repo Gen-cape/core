2eb261cf3b0dcd7ef2033a026be1f1e7e18bd9b3
Boris Polyakov <thewaterbox@yandex.ru>
2025-10-25 16:12:19 +0300

    kanata: use stable

--- areas/augments/kanata/kanata.nix
+++ areas/augments/kanata/kanata.nix
@@ -5,20 +5,21 @@
 }: let
 in {
   environment.systemPackages = [
-    (pkgs.kanata.overrideAttrs (oldAttrs: rec {
-      pname = "kanata";
-      version = "1.8.1";
-      src = pkgs.fetchFromGitHub {
-        owner = "jtroo";
-        repo = pname;
-        rev = "v${version}";
-        sha256 = "sha256-w/PeSqj51gJOWmAV5UPMprntdzinX/IL49D2ZUMfeSM=";
-      };
-      cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-        inherit src;
-        hash = "sha256-T9fZxv3aujYparzVphfYBJ+5ti/T1VkeCeCqWPyllY8";
-      };
-    }))
+    # (pkgs.kanata.overrideAttrs (oldAttrs: rec {
+    #   pname = "kanata";
+    #   version = "1.8.1";
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "jtroo";
+    #     repo = pname;
+    #     rev = "v${version}";
+    #     sha256 = "sha256-w/PeSqj51gJOWmAV5UPMprntdzinX/IL49D2ZUMfeSM=";
+    #   };
+    #   cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+    #     inherit src;
+    #     hash = "sha256-T9fZxv3aujYparzVphfYBJ+5ti/T1VkeCeCqWPyllY8";
+    #   };
+    # }))
+    pkgs.kanata
   ];
 
   services.kanata = {
