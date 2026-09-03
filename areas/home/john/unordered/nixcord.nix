4647b529412a76a81650c3dd182f69daa3617517
Boris Polyakov <thewaterbox@yandex.ru>
2026-09-03 11:34:59 +0300

    nixcord: fix

--- areas/home/john/unordered/nixcord.nix
+++ areas/home/john/unordered/nixcord.nix
@@ -13,22 +13,24 @@
       frameless = false;
       transparent = true;
 
-      # plugins = {
-      #   betterGifPicker.enable = true;
-      #   fakeNitro.enable = true;
-      #   youtubeAdblock.enable = true;
-      #   alwaysTrust.enable = true;
-      #   copyFileContents.enable = true;
-      #   noF1.enable = true;
-      #   showMeYourName.enable = true;
-      #   unindent.enable = true;
-      #   voiceDownload.enable = true;
-      #   voiceMessages.enable = true;
-      #   moreCommands.enable = true;
-      #   moreKaomoji.enable = true;
-      #   imageZoom.enable = true;
-      #   webKeybinds.enable = true;
-      # };
+      plugins = {
+        # betterGifPicker.enable = true;
+        # fakeNitro.enable = true;
+        # youtubeAdblock.enable = true;
+        # alwaysTrust.enable = true;
+        # copyFileContents.enable = true;
+        # noF1.enable = true;
+        # showMeYourName.enable = true;
+        # unindent.enable = true;
+        # voiceDownload.enable = true;
+        # voiceMessages.enable = true;
+        # moreKaomoji.enable = true;
+        # imageZoom.enable = true;
+        # webKeybinds.enable = true;
+        #
+        # Wayland screen-share hook for standard Discord
+        webScreenShareFixes.enable = true;
+      };
     };
   };
 }
