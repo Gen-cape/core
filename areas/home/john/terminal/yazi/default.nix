7f006c2c8125b40b7ad324a2fcbf149a4daf39d1
Boris Polyakov <thewaterbox@yandex.ru>
2026-09-05 15:28:48 +0300

    yazi: explicit EDITOR

--- areas/home/john/terminal/yazi/default.nix
+++ areas/home/john/terminal/yazi/default.nix
@@ -15,5 +15,53 @@
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
+    settings = {
+      opener = {
+        edit = [
+          {
+            run = ''nvim "$@"'';
+            block = true;
+            for = "unix";
+          }
+        ];
+      };
+      open = {
+        prepend_rules = [
+          # Matches standard text files, source code, configs, and JSON/XML/scripts
+          {
+            mime = "text/*";
+            use = "edit";
+          }
+          {
+            mime = "application/x-subrip";
+            use = "edit";
+          }
+          {
+            mime = "application/*json";
+            use = "edit";
+          }
+          {
+            mime = "application/javascript";
+            use = "edit";
+          }
+          {
+            mime = "application/xml";
+            use = "edit";
+          }
+          {
+            mime = "application/x-yaml";
+            use = "edit";
+          }
+          {
+            mime = "application/toml";
+            use = "edit";
+          }
+          {
+            mime = "application/x-shellscript";
+            use = "edit";
+          }
+        ];
+      };
+    };
   };
 }
