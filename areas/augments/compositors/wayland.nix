9c60ef83efd86efcd98bcfc1386c50eb55cee911
Boris Polyakov <thewaterbox@yandex.ru>
2026-09-05 19:39:41 +0300

    wayland: fix discord streaming

--- areas/augments/compositors/wayland.nix
+++ areas/augments/compositors/wayland.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   # Force Electron/Chromium apps to run natively on Wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
@@ -6,6 +10,25 @@
     enable = true;
     wlr.enable = true;
     extraPortals = [pkgs.xdg-desktop-portal-gtk];
-    config.common.default = ["wlr" "gtk"];
+    config = {
+      common.default = ["wlr" "gtk"];
+      mango = {
+        default = ["gtk"];
+        "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+        "org.freedesktop.impl.portal.Screenshot" = "wlr";
+      };
+    };
   };
+
+  systemd.user.services.xdg-desktop-portal.unitConfig = {
+    Requisite = lib.mkForce [];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vesktop
+
+    (discord.override {
+      withOpenASAR = true;
+    })
+  ];
 }
