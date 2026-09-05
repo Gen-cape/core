5d01b810083333bee11ccb90ff9c7e07d1f566ef
Boris Polyakov <thewaterbox@yandex.ru>
2026-09-06 00:33:41 +0300

    fzf: add fish integration

--- areas/home/john/terminal/fish.nix
+++ areas/home/john/terminal/fish.nix
@@ -4,30 +4,35 @@
     pkgs.grc
     pkgs.fd
   ];
-  programs.fish = {
-    enable = true;
+  programs = {
+    fzf = {
+      enableFishIntegration = true;
+    };
+    fish = {
+      enable = true;
 
-    interactiveShellInit = ''
-      set fish_greeting # Disable greeting
-    '';
-    plugins = let
-      plug = name: {
-        name = "${name}";
-        src = pkgs.fishPlugins.${name}.src;
-      };
-    in [
-      (plug "grc")
-      (plug "fzf-fish")
-      (plug "hydro")
-      (plug "z")
-      (plug "fifc")
-      (plug "done")
-      (plug "bass")
-      (plug "sponge")
-      (plug "pisces")
-      (plug "puffer")
-      (plug "clownfish")
-      (plug "fifc")
-    ];
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+      plugins = let
+        plug = name: {
+          name = "${name}";
+          src = pkgs.fishPlugins.${name}.src;
+        };
+      in [
+        (plug "grc")
+        (plug "fzf-fish")
+        (plug "hydro")
+        (plug "z")
+        (plug "fifc")
+        (plug "done")
+        (plug "bass")
+        (plug "sponge")
+        (plug "pisces")
+        (plug "puffer")
+        (plug "clownfish")
+        (plug "fifc")
+      ];
+    };
   };
 }
