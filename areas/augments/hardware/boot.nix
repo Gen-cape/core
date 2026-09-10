32fbcbd952d21d4197a2127cf081369a4c3ed32d
Boris Polyakov <thewaterbox@yandex.ru>
2026-09-10 16:54:29 +0300

    grub: add sidonia theme

--- areas/augments/hardware/boot.nix
+++ areas/augments/hardware/boot.nix
@@ -1,17 +1,37 @@
-{
-  boot = {
-    # loader = {
-    #   systemd-boot.enable = true;
-    #   systemd-boot.configurationLimit = 5;
-    #   efi.canTouchEfiVariables = true;
-    #   timeout = 1;
-    # };
+{pkgs, ...}: let
+  sidoniaTheme = pkgs.stdenvNoCC.mkDerivation {
+    pname = "sidonia-grub-theme-t4-1080p";
+    version = "1.1.0";
 
+    src = pkgs.fetchFromGitHub {
+      owner = "Aleph1-9012";
+      repo = "Sidonia";
+      rev = "master";
+      hash = "sha256-9gnLWD0cT/rz5oGbsnBi9ky5NG2ELkFgV5Lrr0gCXnA=";
+    };
+
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p "$out"
+      cp -r themes/T4/1080p/. "$out/"
+      runHook postInstall
+    '';
+  };
+in {
+  boot = {
     loader.grub = {
+      enable = true;
       efiSupport = true;
       efiInstallAsRemovable = true;
       device = "nodev";
+
+      gfxmodeEfi = "1920x1080";
+      theme = sidoniaTheme;
+      font = "${sidoniaTheme}/fonts/sidonia-t4-1080p.pf2";
     };
+
     initrd.verbose = false;
     consoleLogLevel = 0;
     kernelParams = [
