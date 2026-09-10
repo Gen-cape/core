dd4912027bb57f39b4ac6fc0b4763504f9e2578b
Boris Polyakov <thewaterbox@yandex.ru>
2026-09-10 16:54:29 +0300

    fonts: add departure and monaspace

--- areas/augments/software/common.nix
+++ areas/augments/software/common.nix
@@ -1,9 +1,11 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-color-emoji
     nerd-fonts.jetbrains-mono
+    nerd-fonts.monaspace
+    nerd-fonts.departure-mono
   ];
 
   services.tailscale = {
