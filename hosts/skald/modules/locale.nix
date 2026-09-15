e203f2c5117f37f6f895c7d1673c08d600e67ca7
Boris Polyakov <thewaterbox@yandex.ru>
2026-09-15 15:04:31 +0300

    locale: fix

--- hosts/skald/modules/locale.nix
+++ hosts/skald/modules/locale.nix
@@ -12,18 +12,8 @@
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "ru_RU.UTF-8";
-      LC_IDENTIFICATION = "ru_RU.UTF-8";
-      LC_MEASUREMENT = "ru_RU.UTF-8";
-      LC_MONETARY = "ru_RU.UTF-8";
-      LC_NAME = "ru_RU.UTF-8";
-      LC_NUMERIC = "ru_RU.UTF-8";
-      LC_PAPER = "ru_RU.UTF-8";
-      LC_TELEPHONE = "ru_RU.UTF-8";
-      LC_TIME = "ru_RU.UTF-8";
-    };
     supportedLocales = lib.mkDefault [
+      "C.UTF-8/UTF-8"
       "en_US.UTF-8/UTF-8"
       "ru_RU.UTF-8/UTF-8"
     ];
